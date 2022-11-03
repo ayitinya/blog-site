6806869ef0ce89d62069440c9e603dd0671151f9
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-11-03 00:12:39 +0000

    chore: deletes old files

--- server/api/posts/[id].get.ts
+++
@@ -1,8 +0,0 @@
-import { storageBucket, parseMarkdown, Storage } from "~~/server/utils";
-
-export default defineEventHandler(async _event => {
-    const storage = new Storage()
-    const data = await storage.bucket(storageBucket.name).file('README.md').download()
-    
-    return parseMarkdown(data.toString())
-})
--- server/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as parseMarkdown } from './parseMarkdown'
-export {storageBucket, Storage} from './firebase'
