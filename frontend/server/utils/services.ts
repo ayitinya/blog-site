73f2f635fb5d9e8b582f6b801b8ff75bdf381d22
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-11-09 15:16:07 +0000

    fix: baseURL made to https

--- frontend/server/utils/services.ts
+++ frontend/server/utils/services.ts
@@ -1,4 +1,4 @@
 
 
-export const baseURL = 'http://rasulley.pythonanywhere.com/api/'
+export const baseURL = 'https://rasulley.pythonanywhere.com/api/'
 // export const baseURL = 'http://localhost:8000/api'
