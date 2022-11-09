eca3587664940e5287563c3ff3e12085f43863b9
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-11-09 15:06:36 +0000

    feat: added static_root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.UserProfile"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
