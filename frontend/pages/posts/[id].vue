ea5feed795dd44fab05cbe48a1938f952cf267cb
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-11-10 15:27:04 +0000

    style: aesthetic changes

--- frontend/pages/posts/[id].vue
+++ frontend/pages/posts/[id].vue
@@ -6,17 +6,15 @@ const { data } = await useFetch(`/api/blogs/${useRoute().params.id}`, { key: `bl
     <div class="h-full">
         <NuxtLayout name="custom">
             <template #left>
-                <Icon name="fluent:share-16-filled" size="1.5rem" />
             </template>
             <template #default>
-                <div v-if="data" class="p-10 bg-white rounded-t-lg border border-gray-400">
+                <div v-if="data" class="p-10 bg-white rounded-lg border">
                     <h1>{{ data.title }}</h1>
                     <h2 class="text-slate-500 text-xl">{{ data.subtitle }}</h2>
                     <div id="readme" v-html="data.body" class=""></div>
                 </div>
             </template>
             <template #right>
-                fdadfad
             </template>
         </NuxtLayout>
     </div>
