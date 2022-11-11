3b4d3f609a5cdb846c9404fa486439d904aa7afb
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-11-11 03:48:29 +0000

    feat: added ability to save blog post

--- backend/user/models.py
+++ backend/user/models.py
@@ -3,5 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class UserProfile(AbstractUser):
     pass
+
+
+class SavedBlogs(models.Model):
+    blogs = models.ManyToManyField(to="blog.BlogPost")
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
